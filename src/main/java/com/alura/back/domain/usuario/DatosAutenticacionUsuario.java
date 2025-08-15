6b09f64369e94181f90ace3d4958038766e64a52
martindipeco <martindipeco@gmail.com>
2025-08-15 20:30:07 -0300

    agrego interface de usuario y datosautenticacion

---
+++ src/main/java/com/alura/back/domain/usuario/DatosAutenticacionUsuario.java
@@ -0,0 +1,4 @@
+package com.alura.back.domain.usuario;
+
+public record DatosAutenticacionUsuario() {
+}
---
+++ src/main/java/com/alura/back/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,4 @@
+package com.alura.back.domain.usuario;
+
+public interface UsuarioRepository {
+}
